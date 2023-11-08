afa3393a4c4a6cf24471fdeac82396cffd04a3a0
FedeYozora <federicobonfi@gmail.com>
2023-11-08 15:10:05 +0100

    first commit

---
+++ Script.js
@@ -0,0 +1,23 @@
+fetch("https://striveschool-api.herokuapp.com/books")
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(book => {
+      let card = `<div class="col-6 col-sm-6 clo-md-4 col-lg-4 col-xl-3 mb-4">
+                  <div class="card h-100">
+                     <img src="${book.img}" class="card-img-top img-responsive" alt="${book.title}">
+                     <div class="card-body">
+                        <h5 class="card-title">${book.title}</h5>
+                        <p class="card-text">Prezzo: ${book.price}</p>
+                         <button class="btn btn-primary" id="discard">Scarta</button>
+                     </div>
+                    </div>
+                </div>`;
+      document.querySelector(".row").innerHTML += card;
+    });
+  });
+
+document.addEventListener("click", function (event) {
+  if (event.target.matches("#discard")) {
+    event.target.closest(".col").remove();
+  }
+});
